67690d0b94e0ba875b634debec858dbebc994375
justerik <sebo.erik@gmail.com>
2026-09-10 17:29:18 +0200

    Fixed an issue where the tip text would not move with the yOffset config option, and changed the default yOffset to 0, version is now 1.1.0

--- src/main/java/net/withrage/cozytips/mixin/client/GameMenuScreenMixin.java
+++ src/main/java/net/withrage/cozytips/mixin/client/GameMenuScreenMixin.java
@@ -52,7 +52,7 @@ public class GameMenuScreenMixin {
         int titleColor = ColorUtils.parseColor(config.titleColor);
         int textColor = ColorUtils.parseColor(config.textColor);
 
-        int baseY = screenHeight - 45 - totalTipHeight;
+        int baseY = screenHeight - 45 - totalTipHeight - config.yOffset;
 
         String title = config.title;
         int titleX = (screenWidth - textRenderer.getWidth(title)) / 2;
